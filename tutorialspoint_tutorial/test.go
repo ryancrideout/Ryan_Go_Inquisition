8b1a135e697da20bac7485acf223cdee976f9db9
ryancrideout <ryancrideout@gmail.com>
2023-02-25 11:58:40 -0700

    First batch of tutorials.

---
+++ tutorialspoint_tutorial/test.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* This is NOT my first sample program. */
+	fmt.Println("Hello, World!")
+	/* Yeah don't need it, it's like Python baybee */
+	fmt.Println("Do we not need the ; thing? Maybe not. Nope, we don't!")
+
+	var x float64
+	x = 20.0
+	fmt.Println(x)
+	fmt.Printf("x is of type %T\n", x)
+}
