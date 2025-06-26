13a6c84bd0e32572a334383cae4a3102d0bd8cd1
Ryan Rideout <ryan.rideout@trialcard.com>
2025-06-26 08:25:25 -0600

    Changed the script a bit.

--- website/web_scrapers/go/main.go
+++ website/web_scrapers/go/main.go
@@ -35,38 +35,4 @@ func main() {
 	}
 
 	fmt.Println(htmlContent)
-
-	// // Create a custom HTTP client with longer timeout and that follows redirects
-	// client := &http.Client{
-	// 	Timeout: 10 * time.Second,
-	// }
-
-	// // Create request
-	// req, err := http.NewRequest("GET", hostURL, nil)
-	// if err != nil {
-	// 	fmt.Printf("Error creating request: %v\n", err)
-	// 	return
-	// }
-
-	// // Add headers that curl might be using
-	// req.Header.Set("User-Agent", "Go-http-client/1.1")
-	// req.Header.Set("Accept", "*/*")
-
-	// // Make the request
-	// response, err := client.Do(req)
-	// if err != nil {
-	// 	fmt.Printf("Error making request: %v\n", err)
-	// 	return
-	// }
-	// defer response.Body.Close()
-
-	// // Read the response body
-	// body, err := io.ReadAll(response.Body)
-	// if err != nil {
-	// 	fmt.Printf("Error reading response: %v\n", err)
-	// 	return
-	// }
-
-	// fmt.Println("\nResponse Body:")
-	// fmt.Println(string(body))
 }
