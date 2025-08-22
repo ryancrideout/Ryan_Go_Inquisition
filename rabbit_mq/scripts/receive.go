1424f7540c390031ec7fffb50b32ce3201d3ddd7
Ryan Rideout <ryan.rideout@trialcard.com>
2025-08-22 14:28:07 -0600

    RabbitMQ is setup and good to go, need to implement Quorum Queues to play around with.

--- rabbit_mq/scripts/receive.go
+++ rabbit_mq/scripts/receive.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, message string) {
+	if err != nil {
+		log.Panicf("%s: %s", message, err)
+	}
+}
 
 func main() {
-	fmt.Println("Receive.go exists within this current reality.")
+	/*
+		Notes:
+		1) First we establish a connection
+		2) Then we open a channel
+		3) Then we declare a queue
+		4) Then we consume messages from the queue.
+	*/
+	connection, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer connection.Close()
+
+	channel, err := connection.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer channel.Close()
+
+	queue, err := channel.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	messages, err := channel.Consume(
+		queue.Name, // queue
+		"",         // consumer
+		true,       // auto-ack
+		false,      // exclusive
+		false,      // no-local
+		false,      // no-wait
+		nil,        // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	var forever chan struct{}
+
+	go func() {
+		for message := range messages {
+			log.Println("Received a message:", string(message.Body))
+		}
+	}()
+
+	log.Println("[*] Waiting for messages. To exit press CTRL+C.")
+
+	<-forever
 }
--- rabbit_mq/scripts/send.go
+++ rabbit_mq/scripts/send.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, message string) {
+	if err != nil {
+		log.Panicf("%s: %s", message, err)
+	}
+}
 
 func main() {
-	fmt.Println("Send.go exists!")
+	/*
+		Notes:
+		1) First we open a connection
+		2) Then we open a channel
+		3) Then we open a queue
+		4) Then we publish messages
+
+		RabbitMQ tutorials can be found here: https://www.rabbitmq.com/tutorials
+	*/
+	connection, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer connection.Close() // This defer is called LAST
+
+	channel, err := connection.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer channel.Close() // This defer is called SECOND LAST
+
+	queue, err := channel.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	body := "Today, we feast in HECK!"
+	err = channel.PublishWithContext(ctx,
+		"",         // exchange
+		queue.Name, // routing key
+		false,      // mandatory
+		false,      // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		},
+	)
+	failOnError(err, "Failed to publish a message")
+	log.Println("[x] Sent", body)
+
 }
